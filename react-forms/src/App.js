a11d6589ad6353e70fcc1dcc1c544248f7c3c508
Tiffany <sunngreen@gmail.com>
2016-11-04 21:44:21 -0700

    added data

--- react-forms/src/App.js
+++ react-forms/src/App.js
@@ -6,32 +6,56 @@ import './App.css';
 const MainInterface = React.createClass({
   getInitialState: function(){
     return {
-      title: "Appointments",
-      show: true 
+      data: [
+        {
+          petName: "Buffy",
+          ownerName: "Hassum Harrod",
+          aptDate: "2016-06-20 15:30",
+          aptNotes: "This Chihuahua has not eaten for three days and is lethargic"
+        },
+        {
+          petName: "Spot",
+          ownerName: "Constance Smith",
+          aptDate: "2016-06-24 08:30",
+          aptNotes: "This German Shepherd is having some back pain"
+        },
+        {
+          petName: "Goldie",
+          ownerName: "Barot Bellingham",
+          aptDate: "2016-06-22 15:50",
+          aptNotes: "This Goldfish has some weird spots in the belly"
+        },
+        {
+          petName: "Mitten",
+          ownerName: "Hillary Goldwyn",
+          aptDate: "2016-06-21 9:15",
+          aptNotes: "Cat has excessive hairballs"
+        }
+        ]
     }
+
+
   },
 
   render: function(){
-    var showTitle; 
-    if (this.state.show) {
-      showTitle = 'New '
-    }
-
-    var displayList = {
-      display: this.state.show ? 'block' : 'none', 
-      color: 'red'
-    }
+   
 
     return (
       <div className='interface'>
-        <h1>{showTitle} {this.state.title}</h1>
-        <ul style={displayList}>
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-          <li>Item 4</li>
-
-        </ul>
+        <ul className='item-list media-list'>
+          <li className='pet-item media'>
+            <div className='pet-info media-body'>
+              <div className='pet-head'>
+                <span className='pet-name'>{this.state.data[0].petName}</span>
+                <span className='apt-date pull-right'>{this.state.data[0].aptDate}</span>
+              </div>
+              <div className='owner-name'>
+                <span className='label-item'> Owner: </span> {this.state.data[0].ownerName}
+              </div>
+              <div className='apt-notes'>{this.state.data[0].aptNotes}</div>
+            </div>
+          </li>
+        </ul>  
       </div>
       )
   }, 
